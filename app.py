4ab574382aa70df65034f7fe2fdc3127ece58de6
Martin Curran <martincurran28@hotmail.com>
2019-04-11 12:57:08 +0000

    Created a connection between my MongoDB collections and the Flask application.

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "cookbook"
+app.config["MONGO_URI"] = "mongodb://<dbuser>:<dbpassword>@ds135776.mlab.com:35776/cookbook"
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello world... get ready for a brand new cookbook!'
+@app.route('get_recipe')
+def get_recipe():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    
+
+#@app.route('/')
+#def hello():
+#    return 'Hello world... get ready for a brand new cookbook!'
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
