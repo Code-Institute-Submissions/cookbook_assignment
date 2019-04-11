2908c7512b7f6ec97e3f2e5e20ed35febdcccdbf
Martin Curran <martincurran28@hotmail.com>
2019-04-11 13:16:57 +0000

    Successfully connected to the MongoDB database using Flask and displayed it to the browser using a HTML template.

--- app.py
+++ app.py
@@ -5,20 +5,15 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = "cookbook"
-app.config["MONGO_URI"] = "mongodb://<dbuser>:<dbpassword>@ds135776.mlab.com:35776/cookbook"
+app.config["MONGO_URI"] = "mongodb://admin:7hayfield@ds135776.mlab.com:35776/cookbook"
 
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('get_recipe')
+@app.route('/get_recipe')
 def get_recipe():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
-    
 
-#@app.route('/')
-#def hello():
-#    return 'Hello world... get ready for a brand new cookbook!'
-    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
