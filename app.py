3e5f21393142995af290f7269101eb3087e32d4c
Martin Curran <martincurran28@hotmail.com>
2019-04-12 21:10:57 +0000

    Implemented an edition function for users to edit the elements of their recipes.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -25,16 +26,28 @@ def insert_recipe():
 def get_recipe():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
     
-@app.route('/edit_recipe')
+@app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
-    the_recipe=mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    all_categories= mongo.db.cateogories.find()
-    return render_template("edit_recipe.html", recipe=the_recipe, categories=all_categories)
+    the_recipe = mongo.db.recipes.find_one({"_id":ObjectId(recipe_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edit_recipe.html', recipe=the_recipe, categories=all_categories)
     
+@app.route('/update_recipe/<recipe_id>', methods=['POST'])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update( {"_id": ObjectId(recipe_id)},
+    {
+        'category_name':request.form.get('category_name'),
+        'recipe_name':request.form.get('recipe_name'),
+        'recipe_author':request.form.get('recipe_author'),
+        'date':request.form.get('date'),
+        'gluten_free':request.form.get('gluten_free'),
+    })
     
+    return redirect(url_for('get_recipe'))
     
-
 if __name__ == '__main__':
+    app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
